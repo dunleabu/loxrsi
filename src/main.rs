6da6e09f045d9eca41bc04c5fb6b570100ddc52f
Andrew Dunleavy <37403225+dunleabu@users.noreply.github.com>
2026-06-07 20:29:34 +1200

    setup project - read file from path

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::env;
+use std::fs;
+
+#[derive(Debug)]
+struct Args {pub path: String}
+
+type Res<T> = Result<T, String>;
+
+fn parse_args() -> Res<Args> {
+    let mut args: Vec<_> = env::args().collect();
+    if args.len() == 2 {
+        Ok(Args{path: args.pop().unwrap()})
+    } else {
+        Err("usage: lox <path>".to_string())
+    }
+
+  
+
+
+}
+
+fn read_file(args: Args) -> Res<String> {
+    match fs::read_to_string(args.path) {
+        Ok(text) => Ok(text),
+        Err(e) => Err(format!("{}", e))
+    }
+}
+
+fn main() -> Res<()> {
+    let args = parse_args()?;
+    let text = read_file(args)?;
+    println!(" -- {:?}", text);
+    Ok(())
+}
