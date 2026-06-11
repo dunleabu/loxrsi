8ebfc61b353dd1d09897b762a06024c4a3401782
Andrew Dunleavy <37403225+dunleabu@users.noreply.github.com>
2026-06-11 20:44:22 +1200

    refactor args to switch between execute file and repl (not implemented)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::io;
 use std::str::CharIndices;
 
 type Res<T> = Result<T, String>;
@@ -9,23 +10,32 @@ mod lex;
 use lex::{Token, TokenContext};
 
 #[derive(Debug)]
-struct Args {
-    pub path: String,
+struct FileArgs {
+    pub path: Option<String>,
+}
+
+struct ReplArgs {}
+
+enum Args {
+    File(FileArgs),
+    Repl(ReplArgs),
 }
 
 fn parse_args() -> Res<Args> {
     let mut args: Vec<_> = env::args().collect();
     if args.len() == 2 {
-        Ok(Args {
-            path: args.pop().unwrap(),
-        })
+        Ok(Args::File(FileArgs {
+            path: Some(args.pop().unwrap()),
+        }))
+    } else if args.len() == 1 {
+        Ok(Args::Repl(ReplArgs {}))
     } else {
         Err("usage: lox <path>".to_string())
     }
 }
 
-fn read_file(args: Args) -> Res<String> {
-    match fs::read_to_string(args.path) {
+fn read_file(args: FileArgs) -> Res<String> {
+    match fs::read_to_string(args.path.unwrap()) {
         Ok(text) => Ok(text),
         Err(e) => Err(format!("{}", e)),
     }
@@ -33,7 +43,6 @@ fn read_file(args: Args) -> Res<String> {
 
 fn print_errors(errors: &Vec<TokenContext>) -> String {
     let mut count = 0;
-    println!("");
     for error in errors.iter() {
         match error {
             TokenContext {
@@ -55,13 +64,29 @@ fn print_tokens(tokens: &Vec<TokenContext>) {
         println!("{}, {} : {:?}", tc.line, tc.pos, tc.token)
     }
 
+    println!("");
 }
 
-fn main() -> Res<()> {
-    let args = parse_args()?;
+fn run_file(args: FileArgs) -> Res<()> {
     let text = read_file(args)?;
     match lex::lex(text) {
         Err(errors) => Err(print_errors(&errors)),
         Ok(tokens) => Ok(print_tokens(&tokens)),
+    }
+}
+
+fn run_repl(args: ReplArgs) -> Res<()> {
+    panic!("no repl");
+    let mut input = String::new();
+    loop {
+        //std::io
+    }
+}
+
+fn main() -> Res<()> {
+    println!("");
+    match parse_args()? {
+        Args::File(args) => run_file(args),
+        Args::Repl(args) => run_repl(args),
     }
 }
